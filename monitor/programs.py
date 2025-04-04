f44b54bfcf8c7ac07c7b48e347fa86326a0d22c9
Teddy <etc.tortorici@gmail.com>
2025-04-04 15:19:49 -0600

    model number
    now can choose model number from terminal call

--- monitor/programs.py
+++ monitor/programs.py
@@ -2,12 +2,21 @@ import monitor.main as monitor
 from monitor.communication.server import GpibServer
 from threading import Thread
 import sys
+import argparse
 from PySide6.QtWidgets import QApplication
 
 
 def lakeshore():
     """Launch Server"""
-    server = GpibServer(None, int(340))
+    parser = argparse.ArgumentParser(
+        prog="lakeshore",
+        description="Launch monitoring program for LakeShore",
+        epilog="author: Teddy Tortorici <edward.tortorici@colorado.edu>"
+    )
+    parser.add_argument("model_number", type=int, default=340,
+                        help="What is the model number for the LakeShore (331 or 340)?")
+    args = parser.parse_args()
+    server = GpibServer(None, args.model_number)
     server_thread = Thread(target=server.run, args=())
     server_thread.daemon = True
     server_thread.start()
